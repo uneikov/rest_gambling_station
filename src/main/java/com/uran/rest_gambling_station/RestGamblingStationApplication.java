915c6b35861d66228d4fa1a87e5319af02b9e3f0
IOURI NEIKOV <neikov@gmail.com>
2017-02-07 19:25:53 +0300

    Initial commit

---
+++ src/main/java/com/uran/rest_gambling_station/RestGamblingStationApplication.java
@@ -0,0 +1,12 @@
+package com.uran.rest_gambling_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGamblingStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGamblingStationApplication.class, args);
+	}
+}
